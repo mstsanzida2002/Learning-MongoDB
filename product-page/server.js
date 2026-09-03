f3ba8a62297abcbe31d9adad8413f57399568f0c
Mst. Sanzida <22303252@iubat.edu>
2026-09-03 07:49:48 +0600

    Connect to MongoDB Atlas via Mongoose

--- product-page/server.js
+++ product-page/server.js
@@ -1,6 +1,10 @@
+import 'dotenv/config';
 import app from './src/app.js';
+import { connectDB } from './src/config/db.js';
 
 const PORT = process.env.PORT || 3000;
+
+await connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
